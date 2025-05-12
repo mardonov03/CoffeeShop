9c85de76894dda6ad4acb88b414f785ce00d6a2d
mardonov03 <125536221+mardonov03@users.noreply.github.com>
2025-05-12 23:27:25 +0500

    cart log+ 1

--- internal/repository/postgresql/cart.py
+++ internal/repository/postgresql/cart.py
@@ -4,7 +4,7 @@ from internal.models import menu
 
 class CartRepository:
     def __init__(self, pool):
-        self.pool=pool
+        self.pool = pool
 
     async def get_cart_products(self, cartid: int):
         try:
@@ -14,9 +14,10 @@ class CartRepository:
 
                 products = [menu.ProductInfo(**dict(row)) for row in cart_products]
 
-                return cart.GetCart(cartid=cart_row['cartid'],userid=cart_row['userid'],added_time=cart_row['added_time'],products=products)
+                return cart.GetCart(cartid=cart_row['cartid'], userid=cart_row['userid'], added_time=cart_row['added_time'], products=products)
         except Exception as e:
             logger.error(f'[get_cart_products error]: {e}')
+            raise
 
     async def add_product(self, model: cart.AddProduct):
         try:
@@ -24,6 +25,7 @@ class CartRepository:
                 await conn.execute("INSERT INTO cart_products (cartid, productid, quantity) VALUES ($1, $2, $3)", model.cartid, model.productid, model.quantity)
         except Exception as e:
             logger.error(f'[add_product error]: {e}')
+            raise
 
     async def edit_product(self, model: cart.EditCart):
         try:
@@ -31,13 +33,15 @@ class CartRepository:
                 await conn.execute("UPDATE cart_products SET productid = $1, quantity = $2 WHERE cartid = $3", model.productid, model.quantity, model.cartid)
         except Exception as e:
             logger.error(f'[edit_product error]: {e}')
+            raise
 
     async def delete_product(self, model: cart.DeleteProduct):
         try:
             async with self.pool.acquire() as conn:
-                await conn.execute("DELETE FROM cart_products WHERE productid =$1 AND cartid = $2", model.productid, model.cartid)
+                await conn.execute("DELETE FROM cart_products WHERE productid = $1 AND cartid = $2", model.productid, model.cartid)
         except Exception as e:
             logger.error(f'[delete_product error]: {e}')
+            raise
 
     async def delete_all_products(self, model: cart.Clear):
         try:
@@ -45,3 +49,4 @@ class CartRepository:
                 await conn.execute("DELETE FROM cart_products WHERE cartid = $1", model.cartid)
         except Exception as e:
             logger.error(f'[delete_all_products error]: {e}')
+            raise
--- internal/service/cart.py
+++ internal/service/cart.py
@@ -5,40 +5,55 @@ from fastapi import HTTPException
 
 class CartService:
     def __init__(self, pool, service: UserService):
-        self.repo=CartRepository(pool)
-        self.user_service=service
+        self.repo = CartRepository(pool)
+        self.user_service = service
 
     async def get_cart_products(self, cartid: int, gmail: str):
         result = await self.user_service.is_cart_belongs(gmail, cartid)
 
         if not result:
             raise HTTPException(status_code=403, detail="You do not have permission")
-        return await self.repo.get_cart_products(cartid)
+        try:
+            return await self.repo.get_cart_products(cartid)
+        except Exception:
+            raise HTTPException(status_code=500, detail="Error while receiving the cart")
 
     async def add_product(self, model: cart.AddProduct, gmail: str):
         result = await self.user_service.is_cart_belongs(gmail, model.cartid)
 
         if not result:
             raise HTTPException(status_code=403, detail="You do not have permission")
-        return await self.repo.add_product(model)
+        try:
+            await self.repo.add_product(model)
+        except Exception:
+            raise HTTPException(status_code=500, detail="Error adding product")
 
     async def edit_product(self, model: cart.EditCart, gmail: str):
         result = await self.user_service.is_cart_belongs(gmail, model.cartid)
 
         if not result:
             raise HTTPException(status_code=403, detail="You do not have permission")
-        return await self.repo.edit_product(model)
+        try:
+            await self.repo.edit_product(model)
+        except Exception:
+            raise HTTPException(status_code=500, detail="Error while editing product")
 
     async def delete_product(self, model: cart.DeleteProduct, gmail: str):
         result = await self.user_service.is_cart_belongs(gmail, model.cartid)
 
         if not result:
             raise HTTPException(status_code=403, detail="You do not have permission")
-        return await self.repo.delete_product(model)
+        try:
+            await self.repo.delete_product(model)
+        except Exception:
+            raise HTTPException(status_code=500, detail="Error removing product")
 
     async def delete_all_products(self, model: cart.Clear, gmail: str):
         result = await self.user_service.is_cart_belongs(gmail, model.cartid)
 
         if not result:
             raise HTTPException(status_code=403, detail="You do not have permission")
-        return await self.repo.delete_all_products(model)
+        try:
+            await self.repo.delete_all_products(model)
+        except Exception:
+            raise HTTPException(status_code=500, detail="Error emptying recycle bin")
